90626659b981c609f7d7d449c7d37aba467e04f5
ihaddou <ihaddou@btslivh.eu>
2018-12-18 17:21:10 +0100

    ajout cout

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g1
+ *
+ * Created on 18 décembre 2018, 17:16
+ */
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout <<"bonjour git";
+    return 0;
+}
+
